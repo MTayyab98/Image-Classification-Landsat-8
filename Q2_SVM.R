8f8c717ab3483b1e366fc2d1b406edc93a633d01
MTayyab98 <52675331+MTayyab98@users.noreply.github.com>
2021-10-17 01:13:52 +0530

    Add files via upload

---
+++ Q2_SVM.R
@@ -0,0 +1,36 @@
+setwd("D:\\tayyab\\TERI\\Semester 3\\Geocomputation\\Test3\\LC08_L1TP_146040_20200329_20200409_01_T1.tar\\tayyab")
+
+library(rgdal)
+library(raster)
+library(e1071)
+
+#training data
+TD <- readOGR(dsn = '.', layer = 'traindelhi')
+summary(TD)
+unique(TD$Classname)
+plot(TD)
+
+#composite bands
+
+delhi <- brick('clipped_delhi.tif')
+plotRGB(delhi, r=4,g=3,b=2, stretch='lin')
+plot(TD, add=TRUE)
+
+x <- extract(delhi, TD, df=TRUE)
+names(x)
+dim(x)
+
+x$lc <- as.factor(TD$Classvalue[x$ID])
+x$desc <- as.factor(TD$Classname[x$ID])
+names(x) <- c('ID','b1','b2','b3','b4','lc','desc')
+
+#SVM classification
+
+delhi_svm <- svm(lc~b1+b2+b3+b4, data=x, kernel = 'radial', cost = 4)
+delhi_class <- delhi
+names(delhi_class) <- c('b1','b2','b3','b4')
+delhi_predict <- predict(delhi_class, model=delhi_svm, na.rm=TRUE)
+plot(delhi_predict)
+
+#export map
+writeRaster(delhi_predict, 'delhi_svm.tif', format='GTiff',datatype='INT1U',overwrite=TRUE)
